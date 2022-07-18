c2520a7d64d52caed318e26706c5d9b4fa888798
stringnet <caochen@cloudist.cc>
2022-07-18 10:19:54 +0800

    fix

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -3,36 +3,137 @@ import {
   ReactRNPlugin, 
   AppEvents, 
   RichTextInterface, 
-  useTracker
+  useTracker,
+  useAPIEventListener
 } from '@remnote/plugin-sdk';
 import { log } from '../lib/logging';
 import '../style.css';
 import '../App.css';
 
 async function onActivate(plugin: ReactRNPlugin) {
-  await plugin.settings.registerStringSetting({
-    id: "rule",
-    title: "Rule",
-    defaultValue: "》》::>>,》》》::>>>,：：::\:\:,：：：::\:\:\:,；；::;;,；；；::;;;,《《::<<,！！::!!,￥￥::$$,-》::->",
+  await plugin.settings.registerBooleanSetting({
+    id: "rule_>>",
+    title: "Chinese character conversion：》》to >> and《《 to <<",
+    defaultValue: true,
   });
 
-  // const rule = useTracker(
-  //   async (reactivePlugin) =>
-  //     await reactivePlugin.settings.getSetting("rule")
-  // ) as string;
+  await plugin.settings.registerBooleanSetting({
+    id: "rule_space",
+    title: "Auto add a space between Chinese and English",
+    defaultValue: true,
+  });
+
+  await plugin.settings.registerStringSetting({
+    id: "rule_custom",
+    title: "Custom rule of text hook. (e.g. btw::By the way, AH::At home)",
+    defaultValue: "",
+  });
 
   plugin.event.addListener(AppEvents.EditorTextEdited, undefined, async (newText: RichTextInterface) => {
     const inputText = await plugin.richText.toString(newText);
-    log(plugin, inputText);
-    
-    const widgetCtx = await plugin.widget.getWidgetContext();
-    const dimension = await plugin.widget.getDimensions(widgetCtx["widgetInstanceId"]);
-    log(plugin, JSON.stringify(widgetCtx));
-    log(plugin, JSON.stringify(dimension));
-    if (inputText == "》》") {
-      await plugin.editor.deleteCharacters(2, -1);
-      await plugin.editor.insertMarkdown(">>");
+    const text = inputText.trimEnd();
+
+    // log(plugin, "text:"+text);
+
+    const ruleCustom = String(
+      await plugin.settings.getSetting("rule_custom")
+    );
+
+    if (ruleCustom.length > 0) {
+      const rules = ruleCustom.split(",");
+      for (const rule of rules) {
+        const [src, dst] = rule.split("::");
+        log(plugin, `${text}, ${src}, ${text.includes(src)}`);
+        if (text.includes(src, text.length - src.length)) {
+          await plugin.editor.deleteCharacters(src.length, -1);
+          await plugin.editor.insertMarkdown(dst);
+          break;
+        }
+      }
     }
+
+    const rule1 = Boolean(
+      await plugin.settings.getSetting("rule_>>")
+    );
+    
+    if (rule1) {
+      if (text.charAt(text.length - 1) == '》' && text.charAt(text.length - 2) == '》') {
+        await plugin.editor.deleteCharacters(2, -1);
+        await plugin.editor.insertMarkdown(">>");
+      } else if (text.charAt(text.length - 1) == '《' && text.charAt(text.length - 2) == '《') {
+        await plugin.editor.deleteCharacters(2, -1);
+        await plugin.editor.insertMarkdown("<<");
+      }
+    }
+    const ruleSpace = Boolean(
+      await plugin.settings.getSetting("rule_space")
+    );
+    if (ruleSpace) {
+      const englishPattern = new RegExp("[A-Za-z0-9]+");
+      const chinesePattern = new RegExp("[\u4E00-\u9FA5]+");
+  
+      function getToken(text: string, index: number, pattern: RegExp) {
+        var res = "";
+        var c = index;
+        while (c >= 0) {
+          if (pattern.test(text.charAt(c))) {
+            res += text.charAt(c);
+          } else {
+            break;
+          }
+          c -= 1;
+        }
+        return res.split('').reverse().join('')
+      }
+  
+      do {
+        let preBlackList: string[] = await plugin.storage.getSession("blackList");
+        if (preBlackList != undefined && preBlackList.includes(text)) {
+          log(plugin, JSON.stringify(preBlackList));
+          break;
+        }
+        log(plugin, "--start--");
+        var index = text.length - 1;
+        const rightToken = getToken(text, index, chinesePattern);
+        if (rightToken.length == 0) {
+          break;
+        }
+  
+        // log(plugin, "rightToken:"+rightToken+",index="+index);
+        index -= rightToken.length;
+        const englishToken = getToken(text, index, englishPattern);
+        if (englishToken.length == 0) {
+          break;
+        }
+        // log(plugin, "englishToken:"+englishToken+",index="+index);
+        index -= englishToken.length;
+        const leftToken = getToken(text, index, chinesePattern);
+        if (leftToken.length == 0) {
+          break;
+        }
+        // log(plugin, "leftToken:"+leftToken+",index="+index);
+  
+        var base = text.substring(0, leftToken.length);
+        let blackList: string[] = [];
+        blackList.push(base);
+        for (let item of englishToken + rightToken) {
+          base += item
+          blackList.push(base);
+        }
+        await plugin.storage.setSession("blackList", blackList);
+        log(plugin, JSON.stringify(blackList));
+  
+        const delNum = rightToken.length + englishToken.length;
+        const addStr = " " + englishToken + " " + rightToken;
+        log(plugin, "wow:"+delNum+", "+addStr);
+        await plugin.editor.deleteCharacters(delNum, -1);
+        await plugin.editor.insertMarkdown(addStr);
+        await plugin.storage.setSession("blackList", []);
+        break;
+      } while(1);
+    }
+    
+    
   });
 }
 
