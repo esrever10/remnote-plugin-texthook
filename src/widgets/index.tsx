1203a335c35d587b55d0eaf111f4a593c5e5cc4a
stringnet <caochen@cloudist.cc>
2022-07-23 17:01:48 +0800

    rewrite

--- src/widgets/index.tsx
+++ src/widgets/index.tsx
@@ -32,12 +32,12 @@ async function onActivate(plugin: ReactRNPlugin) {
     const inputText = await plugin.richText.toString(newText);
     const text = inputText.trimEnd();
 
-    // log(plugin, "text:"+text);
+    log(plugin, "text:"+text);
 
+    // For custom rule
     const ruleCustom = String(
       await plugin.settings.getSetting("rule_custom")
     );
-
     if (ruleCustom.length > 0) {
       const rules = ruleCustom.split("\n");
       for (const rule of rules) {
@@ -51,10 +51,10 @@ async function onActivate(plugin: ReactRNPlugin) {
       }
     }
 
+    // For Chinese 》》
     const rule1 = Boolean(
       await plugin.settings.getSetting("rule_>>")
     );
-    
     if (rule1) {
       if (text.charAt(text.length - 1) == '》' && text.charAt(text.length - 2) == '》') {
         await plugin.editor.deleteCharacters(2, -1);
@@ -64,76 +64,102 @@ async function onActivate(plugin: ReactRNPlugin) {
         await plugin.editor.insertMarkdown("<<");
       }
     }
+
+    // For auto space
     const ruleSpace = Boolean(
       await plugin.settings.getSetting("rule_space")
     );
     if (ruleSpace) {
-      const englishPattern = new RegExp("[A-Za-z0-9]+");
-      const chinesePattern = new RegExp("[\u4E00-\u9FA5]+");
-  
-      function getToken(text: string, index: number, pattern: RegExp) {
-        var res = "";
-        var c = index;
-        while (c >= 0) {
-          if (pattern.test(text.charAt(c))) {
-            res += text.charAt(c);
-          } else {
-            break;
-          }
-          c -= 1;
+      let patternMap = new Map([
+        ["english", "[A-Za-z0-9\\~!@#\$%\^&\(\)\_\+-={}']+"],
+        ["chinese", "[\u2E80-\u9FFF]+"],
+      ])
+
+      function getTokenPattern(char:string) {
+        if (char == ' ') {
+          return "space";
         }
-        return res.split('').reverse().join('')
+        const patterns = Array.from(patternMap.keys());
+        for (const p of patterns) {
+          if (new RegExp(patternMap.get(p)!).test(char)) {
+            return p
+          }
+        }
+        return "others";
       }
-  
+
+      function parse(text: string) {
+        if (text.length == 0) {
+          return []
+        }
+        let res: [string, string][] = [];
+        var lastPattern = getTokenPattern(text.charAt(0));
+        var buffer = text.charAt(0);
+        var c = 1;
+        while (c <= text.length) {
+          var newPattern = "";
+          if (c < text.length) {
+            newPattern = getTokenPattern(text.charAt(c));            
+          }
+          if (newPattern != lastPattern) {
+            res.push([lastPattern!, buffer]);
+            buffer = text.charAt(c);
+            lastPattern = newPattern;
+          } else {
+            buffer = buffer.concat(text.charAt(c));
+          }
+          c += 1
+        }
+        return res;
+      }
+      
+      function mergeTokensText(tokens: [string, string][]) {
+        var buffer = '';
+        for (const token of tokens) {
+          buffer += token[1];
+        }
+        return buffer;
+      }
+
+      function addSpace(tokens: [string, string][]): [number, string] {
+        if (tokens.length < 3) {
+          return [0, ""];
+        }
+        const [left, mid, right] = tokens.slice(tokens.length - 3);
+        const delNum = left[1].length + mid[1].length + right[1].length;
+        if ((left[0] == 'chinese' && mid[0] == 'english' && right[0] == 'chinese') ) {
+          return [delNum, left[1] + ' ' + mid[1] + ' ' + right[1]];
+        } else if (left[0] == 'english' && mid[0] == 'chinese' && right[0] == 'english') {
+          return [delNum, left[1] + ' ' + mid[1] + right[1]];
+        } 
+        return [0, ""];
+      }
+      
       do {
         let preBlackList: string[] = await plugin.storage.getSession("blackList");
         if (preBlackList != undefined && preBlackList.includes(text)) {
-          // log(plugin, JSON.stringify(preBlackList));
           break;
         }
-        // log(plugin, "--start--");
-        var index = text.length - 1;
-        const rightToken = getToken(text, index, chinesePattern);
-        if (rightToken.length == 0) {
-          break;
-        }
-  
-        // log(plugin, "rightToken:"+rightToken+",index="+index);
-        index -= rightToken.length;
-        const englishToken = getToken(text, index, englishPattern);
-        if (englishToken.length == 0) {
-          break;
-        }
-        // log(plugin, "englishToken:"+englishToken+",index="+index);
-        index -= englishToken.length;
-        const leftToken = getToken(text, index, chinesePattern);
-        if (leftToken.length == 0) {
-          break;
-        }
-        // log(plugin, "leftToken:"+leftToken+",index="+index);
-  
-        var base = text.substring(0, index + 1);
-        log(plugin, `base:${base}`);
-        let blackList: string[] = [];
-        blackList.push(base);
-        for (let item of englishToken + rightToken) {
-          base += item
+        const tokens = parse(text);
+        log(plugin, "tokens: " + JSON.stringify(tokens));
+        const [delNum, newText] = addSpace(tokens);
+        log(plugin, "newText: " + newText);
+        if (delNum > 0) {
+          let blackList: string[] = [];
+          var base = tokens[0][1];
+          log(plugin, "base: " + base);
           blackList.push(base);
+          for (let item of text.substring(base.length)) {
+            base += item
+            blackList.push(base);
+          }
+          log(plugin, "blacklist: " + JSON.stringify(blackList));
+          await plugin.storage.setSession("blackList", blackList);
+          await plugin.editor.deleteCharacters(delNum, -1);
+          await plugin.editor.insertMarkdown(newText);
         }
-        await plugin.storage.setSession("blackList", blackList);
-        log(plugin, JSON.stringify(blackList));
-  
-        const delNum = rightToken.length + englishToken.length;
-        const addStr = " " + englishToken + " " + rightToken;
-        // log(plugin, "wow:"+delNum+", "+addStr);
-        await plugin.editor.deleteCharacters(delNum, -1);
-        await plugin.editor.insertMarkdown(addStr);
-        // await plugin.storage.setSession("blackList", []);
-        break;
-      } while(1);
+      } while(false);
     }
-    
-    
   });
 }
 
